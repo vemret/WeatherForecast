93a42a312ce95e548685de1f0736a3f4b053146f
Vahit Emre <vemretellier@gmail.com>
2021-12-16 22:15:33 +0300

    Update ViewController.swift
    The weather forecast is retrieved based on the user's location information.

--- WeatherForecast/ViewController.swift
+++ WeatherForecast/ViewController.swift
@@ -51,80 +51,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
 //            location information provided
             locationManager.startUpdatingLocation()
+            
+            print(userLatitude)
+            print(userLongitude)
+            
         }
 //            **********************************************************************************************
         
         
-
-//        1) Request & Session
-//        2) Response & Data
-//        3) Parsing & Json Serialization
         
-        let baseURL = URL(string: "https://api.darksky.net/forecast/\(apiKey)/")
-//        guard let means if it satisfies the condition
-        guard let requestURL = URL(string: "41.0458902,28.960685", relativeTo: baseURL) else { return  }
-        
-//  ***************************************************************
-//        when the internet connection rate is low in other words Json Object don't come synchronously, app will keep working. App is not waiting to come json object quickly for The Actions keep going.
-        let request = URLRequest(url: requestURL)
-//        an object that coordinates a group of related Data transfer / Bu ağa gidip veri transferi yapabilmek için UrlSession oluşturuldu.
-        let session = URLSession(configuration: URLSessionConfiguration.default)
-        print("Control 1")
-        
-//        ****************** - Closure - *****************
-        let task = session.dataTask(with: request) { data, response, error in
-            if error != nil {
-//                  error?.localizedDescription => error description
-                let alert = UIAlertController(title: "Error!", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
-                let ok = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
-                alert.addAction(ok)
-                self.present(alert, animated: true, completion: nil)
-            } else {
-                if (data != nil){
-                    print("Control 2")
-                    
-                    let jsonData = try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
-                    
-//                    ASYNC
-                    DispatchQueue.main.async {
-                        if let weatherLocation = jsonData["timezone"] as? String{
-                            self.locationLabel.text = weatherLocation
-                        }
-//                        print("Control 3")
-                        
-                        if let currentWeatherJson = jsonData["currently"] as? [String : Any]{
-//                           print(currentWeatherJson)
-                            if let tempature = currentWeatherJson["temperature"] as? Double  {
-//                                print(tempature)
-                                if let precipitation = currentWeatherJson["precipProbability"] as? Double{
-                                    if let summary = currentWeatherJson["summary"] as? String{
-                                        if let humidity = currentWeatherJson["humidity"] as? Double{
-                                            if let img = currentWeatherJson["icon"] as? String{
-                                                let weatherForecast = CurrentMood(tempature: tempature, precipitation: precipitation, summary: summary, humidity: humidity, img: img)
-                                                let viewModel = CurrentMoodModal(data: weatherForecast)
-                                                self.showView(model: viewModel)
-                                                
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-//                    print("Control 4")
-                }
-                
-            }
-        }
-//        task started running
-        task.resume()
-//        print("Conrtol 5")
-        
-//        print("Codess keep working..")
-//        ********************************************************
-        
-
-            
         
         
   /*
@@ -143,8 +78,27 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 /*        let weatherForecast = CurrentMood(tempature: 35.32, precipitation: 45.21, summary: "Sunny", humidity: 83.12, img: "rain")
         let viewModel = CurrentMoodModal(data: weatherForecast)
         showView(model: viewModel)*/
-    }
 
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locationValue = CLLocationCoordinate2D.init(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        
+        
+        self.userLongitude = locationValue.longitude
+        self.userLatitude = locationValue.latitude
+        
+        print(userLatitude)
+        print(userLongitude)
+        
+
+//    getData is called here because the coordinate information is retrieved here.
+        getData()
+        
+    }
+    
+    
 //    for show on main screen
     func showView(model : CurrentMoodModal){
         temperatureLabel.text = model.tempature
@@ -152,21 +106,87 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         precipitationLabel.text = model.precipitation
         humidityLabel.text = model.humidity
         weatherImage.image = model.img
-    }
-    
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let locationValue = CLLocationCoordinate2D.init(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
-        
-        userLongitude = locationValue.longitude
-        userLatitude = locationValue.latitude
         
 //        indicater stop
         indicator.stopAnimating()
+        print(userLatitude)
+        print(userLongitude)
     }
     
     
-
+    func getData(){
+        
+        //        1) Request & Session
+        //        2) Response & Data
+        //        3) Parsing & Json Serialization
+                
+                let baseURL = URL(string: "https://api.darksky.net/forecast/\(apiKey)/")
+                
+                
+                
+        //        guard let means if it satisfies the condition
+                guard let requestURL = URL(string: "\(userLatitude),\(userLongitude)", relativeTo: baseURL) else { return  }
+                
+        //  ***************************************************************
+        //        when the internet connection rate is low in other words Json Object don't come synchronously, app will keep working. App is not waiting to come json object quickly for The Actions keep going.
+                let request = URLRequest(url: requestURL)
+        //        an object that coordinates a group of related Data transfer / Bu ağa gidip veri transferi yapabilmek için UrlSession oluşturuldu.
+                let session = URLSession(configuration: URLSessionConfiguration.default)
+                print("Control 1")
+                
+        //        ****************** - Closure - *****************
+                let task = session.dataTask(with: request) { data, response, error in
+                    if error != nil {
+        //                  error?.localizedDescription => error description
+                        let alert = UIAlertController(title: "Error!", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                        let ok = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                        alert.addAction(ok)
+                        self.present(alert, animated: true, completion: nil)
+                    } else {
+                        if (data != nil){
+                            print("Control 2")
+                            
+                            let jsonData = try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
+                            
+        //                    ASYNC
+                            DispatchQueue.main.async {
+                                if let weatherLocation = jsonData["timezone"] as? String{
+                                    self.locationLabel.text = weatherLocation
+                                }
+        //                        print("Control 3")
+                                
+                                if let currentWeatherJson = jsonData["currently"] as? [String : Any]{
+        //                           print(currentWeatherJson)
+                                    if let tempature = currentWeatherJson["temperature"] as? Double  {
+        //                                print(tempature)
+                                        if let precipitation = currentWeatherJson["precipProbability"] as? Double{
+                                            if let summary = currentWeatherJson["summary"] as? String{
+                                                if let humidity = currentWeatherJson["humidity"] as? Double{
+                                                    if let img = currentWeatherJson["icon"] as? String{
+                                                        let weatherForecast = CurrentMood(tempature: tempature, precipitation: precipitation, summary: summary, humidity: humidity, img: img)
+                                                        let viewModel = CurrentMoodModal(data: weatherForecast)
+                                                        self.showView(model: viewModel)
+                                                        
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+        //                    print("Control 4")
+                        }
+                        
+                    }
+                }
+        //        task started running
+                task.resume()
+        //        print("Conrtol 5")
+                
+        //        print("Codess keep working..")
+        //        ********************************************************
+                
+    }
     
 
 }
